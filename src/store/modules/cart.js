bb8d9d3b3ccdcd1f896719630fb7deeacbc2443c
iStef <stefan_b_k@icloud.com>
2022-06-11 20:51:17 +0300

    migrate products and cart

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -1,11 +1,66 @@
-const state = {};
+const state = {
+  items: [],
+  total: 0,
+  qty: 0
+};
 
-const getters = {};
+const getters = {
+  items(state) {
+    return state.items
+  },
+  totalSum(state) {
+    return state.total
+  },
+  quantity(state) {
+    return state.qty
+  }
+};
 
-const mutations = {};
+const mutations = {
+  addProductToCart (state, payload) {
+    const productData = payload.product;
+    const productInCartIndex = state.items.findIndex(
+      (ci) => ci.productId === productData.id
+    );
 
-const actions = {};
+    if (productInCartIndex >= 0) {
+      state.items[productInCartIndex].qty++;
+    } else {
+      const newItem = {
+        productId: productData.id,
+        title: productData.title,
+        image: productData.image,
+        price: productData.price,
+        qty: 1
+      };
+      state.items.push(newItem);
+    }
+    state.qty++;
+    state.total += productData.price;
+  },
+
+  removeProductFromCart (state, payload) {
+    const prodId = payload.productId;
+    const productInCartIndex = state.items.findIndex(
+      (cartItem) => cartItem.productId === prodId
+    );
+    const prodData = state.items[productInCartIndex];
+    state.items.splice(productInCartIndex, 1);
+    state.qty -= prodData.qty;
+    state.total -= prodData.price * prodData.qty;
+  }
+};
+
+const actions = {
+  addToCart ({ commit }, payload) {
+    commit('addProductToCart', payload);
+  },
+  removeFromCart ({ commit }, payload) {
+    commit('removeProductFromCart', payload);
+  }
+};
 
 export default {
+  namespaced: true,
   state, getters, actions, mutations
 };
